7d3c914f3652aed1b023617489299f656065cdb7
Sanzhar Anarbay <anarbay.sanzhar1999@gmail.com>
2020-12-30 16:11:18 +0600

    Fix

--- resources/views/modules/admin/partials/footer.blade.php
+++ resources/views/modules/admin/partials/footer.blade.php
@@ -4,7 +4,7 @@
     <div class="footer-inner bg-white">
         <div class="row">
             <div class="col-sm-6">
-                Copyright &copy;  <span id="year"></span> Geronimo Lap
+                Copyright &copy;  <span id="year"></span> Almaty Museum
             </div>
             <div class="col-sm-6 text-right">
                 Designed by <a href="https://www.instagram.com/sanz8ar/">@sanzhar</a>
